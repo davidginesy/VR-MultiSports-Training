043356871b39029f36d41bf8030d5bf35a4b0d54
nguessan-pierre <n.pierre.alex@gmail.com>
2018-03-21 16:14:48 -0400

    début scoring/stats --> basket

---
+++ Assets/Scripts/BallLife.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLife : MonoBehaviour {
+	public float Timer;
+	public float livingTime;
+
+	void Update(){
+		Timer += Time.deltaTime;
+		if (Timer >= livingTime) {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Scoring.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour {
+	public int count=0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		count += 1;
+		Debug.Log (count);
+	}
+}
