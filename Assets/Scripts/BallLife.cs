999ec1c0843593a38a0c32226e75c845e0ad3388
nguessan-pierre <n.pierre.alex@gmail.com>
2018-04-18 13:44:24 -0400

    ajout sons // basket

--- Assets/Scripts/BallLife.cs
+++ Assets/Scripts/BallLife.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class BallLife : MonoBehaviour {
 	public float Timer;
 	public float livingTime;
+	public AudioSource audio;
 
 	void Update(){
 		Timer += Time.deltaTime;
 		if (Timer >= livingTime) {
-			Destroy (this.gameObject);
+			//Destroy (this.gameObject);
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		Debug.Log ("collision");
+		audio.Play ();
+		if (other.transform.tag.Equals("Decor")){
+			
+			audio.Play();
 		}
 	}
 }
